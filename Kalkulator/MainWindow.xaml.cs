b6a21571d3fbfb7338297f8e472ba7df8107ff73
Vojtech Pejsar <pejsarvojtech@gmail.com>
2021-05-22 23:03:44 +0200

    Přidal jsem základní logiku v vytvořil základní layout aplikace.

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -12,17 +12,72 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Kalkulator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        decimal number;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            number = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox1.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox1.Text);
+            }
+            if (textbox2.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox2.Text);
+
+            }
+            if (textbox3.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox3.Text);
+            }
+
+            resultTextBox.Text = "Výsledek: " + Convert.ToString(number);
+
+            number = 0;
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            decimal result;
+            e.Handled = !decimal.TryParse((sender as TextBox).Text + e.Text, out result);
+        }
+
+        private void textbox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space) e.Handled = true;
+        }
+
+        private void textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textbox1.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox1.Text);
+            }
+            if (textbox2.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox2.Text);
+
+            }
+            if (textbox3.Text != string.Empty)
+            {
+                number += Convert.ToDecimal(textbox3.Text);
+            }
+
+            resultTextBox.Text = "Výsledek: " + Convert.ToString(number);
+
+            number = 0;
         }
     }
 }
