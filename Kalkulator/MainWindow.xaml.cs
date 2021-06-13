c84e0fa67cbd48ab71569f54b7d3e748125a8f8e
Vojtech Pejsar <pejsarvojtech@gmail.com>
2021-06-13 21:03:16 +0200

    dodělal jsem výpočet

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Collections;
+using System.Globalization;
+using System.Reflection;
 
 namespace Kalkulator
 {
@@ -34,25 +37,52 @@ namespace Kalkulator
             if (e.Key == Key.Space) e.Handled = true;
         }
 
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy || e.Command == ApplicationCommands.Cut || e.Command == ApplicationCommands.Paste)
+                e.Handled = true;            
+        }
+
         private void Button_Spocitat(object sender, RoutedEventArgs e)
         {
-            double A = 0;
-            decimal Wo = 0;
-            decimal Wk = 0;
-            double F = 0;
-            decimal M = 0;
+            
+            double numResultA;
+            double numResultWo;            
+            double numResultWk;
+            double numResultF;
+            double numResultM;
 
-            if ((textBoxFy.Text != null || textBoxFy.Text != ",") && (textBoxFx.Text != null || textBoxFx.Text != ","))
+            double numResultSmyk;
+            double numResultOhyb;
+            double numResultKrut;
+
+            if (textBoxFy.Text != "" && textBoxFx.Text != "" && textBoxDo.Text != "" && textBoxDi.Text != "" && textBoxMx.Text != "" && textBoxMy.Text != "")
             {
-                resultF.Text = Convert.ToString(Math.Sqrt(Math.Pow(Convert.ToDouble(textBoxFx.Text), 2) + Math.Pow(Convert.ToDouble(textBoxFy.Text), 2)));
-            }
+                //bugfixing troubleshooting
+                //MessageBox.Show(Convert.ToString(Math.Sqrt(Math.Pow(Convert.ToDouble(textBoxFx.Text), 2) + Math.Pow(Convert.ToDouble(textBoxFy.Text), 2))));
+                //MessageBox.Show(Convert.ToString((Math.PI * (Math.Pow(Convert.ToDouble(textBoxDo.Text), 2) - Math.Pow(Convert.ToDouble(textBoxDi.Text), 2))) / 4));
 
-            if ((textBoxDo.Text != null || textBoxDo.Text != ",") && (textBoxDi.Text != null || textBoxDi.Text != ","))
-            {
-                resultA.Text = Convert.ToString((Math.PI * (Math.Pow(Convert.ToDouble(textBoxDo.Text), 2) - Math.Pow(Convert.ToDouble(textBoxDi.Text), 2))) / 4);
-            }
+                //výpočet
+                numResultF = Math.Sqrt(Math.Pow(Convert.ToDouble(textBoxFx.Text), 2) + Math.Pow(Convert.ToDouble(textBoxFy.Text), 2));
+                numResultA = (Math.PI * (Math.Pow(Convert.ToDouble(textBoxDo.Text), 2) - Math.Pow(Convert.ToDouble(textBoxDi.Text), 2))) / 4;
+                numResultWo = (Math.PI * (Math.Pow(Convert.ToDouble(textBoxDo.Text), 4) - Math.Pow(Convert.ToDouble(textBoxDi.Text), 4))) / (32 * Convert.ToDouble(textBoxDo.Text));
+                numResultWk = numResultWo * 2;
+                numResultM = Math.Sqrt(Math.Pow(Convert.ToDouble(textBoxMx.Text), 2) + Math.Pow(Convert.ToDouble(textBoxMy.Text), 2));
 
-            resultSmyk.Text = Convert.ToString(Convert.ToDecimal(resultF.Text) / Convert.ToDecimal(resultA.Text));
+                numResultSmyk = numResultF / numResultA;
+                numResultOhyb = numResultM / numResultWo;
+                numResultKrut = Convert.ToDouble(textBoxMx.Text) / numResultWk;
+
+                resultA.Text = numResultA.ToString("g5", NumberFormatInfo.CurrentInfo);
+                resultF.Text = numResultF.ToString("g5", NumberFormatInfo.CurrentInfo);
+                resultM.Text = numResultM.ToString("g5", NumberFormatInfo.CurrentInfo);
+                resultWo.Text = numResultWo.ToString("g5", NumberFormatInfo.CurrentInfo);
+                resultWk.Text = numResultWk.ToString("g5", NumberFormatInfo.CurrentInfo);
+
+                resultOhyb.Text = numResultOhyb.ToString("g5", NumberFormatInfo.CurrentInfo);
+                resultKrut.Text = numResultKrut.ToString("g5", NumberFormatInfo.CurrentInfo); 
+                resultSmyk.Text = numResultSmyk.ToString("g5", NumberFormatInfo.CurrentInfo);
+            }
         }
 
         private void Button_Vysvetlivky(object sender, RoutedEventArgs e)
